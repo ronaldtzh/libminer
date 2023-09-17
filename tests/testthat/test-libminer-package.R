93ce829d2e167dad43c7da99777fbd7dfbe532a1
ronaldtzh <ronaldtzh@gmail.com>
2023-09-18 03:17:58 +0800

    test

--- tests/testthat/test-libminer-package.R
+++ tests/testthat/test-libminer-package.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("lib_summary returns expected results", {
+  res <- lib_summary()
+  expect_s3_class(res, "data.frame")
+  expect_equal(ncol(res), 2)
+  expect_equal(names(res), c("Library", "n_packages"))
+  expect_type(res$Library, "character")
+  expect_type(res$n_packages, "integer")
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "unused argument")
 })
