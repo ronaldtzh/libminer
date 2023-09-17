03b8944fd9a9be1ce7bd1ba2f8cf20a233336f31
ronaldtzh <ronaldtzh@gmail.com>
2023-09-18 05:11:11 +0800

    update param

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -2,6 +2,9 @@
 #'
 #' Provides a brief summary of the package libraries on your machine
 #'
+#' @param sizes logical indicating whether or not to calculate
+#'    library sizes. Default `FALSE`.
+#'
 #' @return A `data.frame` containing the count of
 #'    packages in each of the user's libraries
 #' @export
@@ -9,6 +12,11 @@
 #' @examples
 #' lib_summary()
 lib_summary <- function(sizes = FALSE) {
+  if(!is.logical(sizes)){
+    stop("'sizes' must be TRUE or FALSE.")
+  }
+
+
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
